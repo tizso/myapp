505c1d4900f59f3b9f8b3a1283899e992a3ab72f
tizso <zsolt.kabai@categorie.ro>
2022-09-18 11:25:42 +0300

    First commit

---
+++ src/views/Product.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>
+      Product Page
+    </div>
+    <div>
+      Looking for Product: {{ this.$route.params.id }}
+    </div>
+  </div>
+</template>
---
+++ src/views/Profile.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <span>Profile Info</span>
+    <span>
+         {{ this.$route.params.userName }}
+      </span>
+  </div>
+
+</template>
